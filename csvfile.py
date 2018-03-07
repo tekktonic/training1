1e7782c0ad0aa0a82ff7941b27452b702de56051
Colleen Britt <colleencbritt@gmail.com>
2018-03-07 16:21:26 -0500

    CSV reader skips first line
    csvfile.py
      -- Uses a flag to skip the first line of the file
    
    Issue #11

--- csvfile.py
+++ csvfile.py
@@ -3,7 +3,11 @@ import csv
 def generator():
     with open('phonedata.csv', newline='') as csvfile:
         datareader = csv.reader(csvfile)
+        firstline = True
         for row in datareader:
+            if firstline:
+                firstline = False
+                continue
             yield(row)
 
 
