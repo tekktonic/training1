32d43a7a5f563aa043fa4ed8acc14ec07a1304ca
Daniel Wilkins Daniel.Wilkins@kpit.com <Daniel.Wilkins@kpit.com>
2018-03-07 16:25:40 -0500

    Experimental csv support

--- slice.py
+++ slice.py
@@ -17,19 +17,24 @@ class DataSlice(object):
 
     def __str__(self):
         return  ("Accelerometer: " + str(self.accelerometer)
-    + "\nGravity sensor: " + str(self.gravity)
-    + "\nAcceleration: " + str(self.acceleration)
-    + "\nGyroscope: " + str(self.gyroscope)
-    + "\nLight sensor: " + str(self.light)
-    + "\nMagnetic Field sensor: " + str(self.mag_field)
-    + "\nOrentation sensor: " + str(self.orientation)
-    + "\nProximity sensor: " + str(self.proximity)
-    + "\nSound Level: " + str(self.sound)
-    + "\nLocation Data: " + str(self.location)
-    + "\nTime Since Start: " + str(self.time)
-    + "\nDate " + str(self.date_time + "\n"))
+                 + "\nGravity sensor: " + str(self.gravity)
+                 + "\nAcceleration: " + str(self.acceleration)
+                 + "\nGyroscope: " + str(self.gyroscope)
+                 + "\nLight sensor: " + str(self.light)
+                 + "\nMagnetic Field sensor: " + str(self.mag_field)
+                 + "\nOrentation sensor: " + str(self.orientation)
+                 + "\nProximity sensor: " + str(self.proximity)
+                 + "\nSound Level: " + str(self.sound)
+                 + "\nLocation Data: " + str(self.location)
+                 + "\nTime Since Start: " + str(self.time)
+                 + "\nDate " + str(self.date_time + "\n"))
 
-
+    def csv(self):
+        return (",".join(self.accelerometer) +
+                ",".join(self.gravity) +
+                ",".join(self.acceleration) +
+                ",".join(self.gyroscope) +
+                ",".join(self.light))
 def csv_next_slice():
     first_line = True
     for line in generator():
@@ -38,3 +43,6 @@ def csv_next_slice():
         sl = DataSlice(tuple(line[0:3]), tuple(line[3:6]), tuple(line[6:9]), tuple(line[9:12]), line[12], tuple(line[12:15]), tuple(line[15:18]), line[18], line[19], tuple(line[20:27]), line[28], line[29], line[30])
         yield(sl)
 
+for sl in csv_next_slice():
+    sl.csv()
+    break
